2565dbed53cbe4821c96744fe5bb012dfc3ce934
SenthilKumar M <56290175+senthilkumar909542@users.noreply.github.com>
2023-08-29 23:01:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,100 @@
+let boxes = document.querySelectorAll("div.box");
+let player_1 = 'X';
+let player_2 = 'O';
+let current_player = 'X';
+let result = document.querySelector('h1');
+let start_value = document.querySelector("h1#start_value");
+let winning_pattern;
+let winnerIndicator = getComputedStyle(document.body).getPropertyValue('--winning-blocks');
+let array = Array(9).fill(null);
+
+var start_btn = document.getElementById("start");
+
+
+start_btn.addEventListener("click", start);
+
+function start() {
+    start_value.innerHTML = "";
+    started();
+
+}
+
+
+function started() {
+
+    boxes.forEach(box => box.addEventListener('click', boxClicked));
+
+}
+
+
+function boxClicked(e) {
+
+    let index = e.target.id;
+
+
+    if (array[index] === null) {
+        array[index] = e.target.innerHTML = current_player;
+    }
+
+
+    if (playerwon() === true) {
+        result.innerHTML = current_player + " has won";
+
+        winning_pattern.map(pattern => boxes[pattern].style.backgroundColor = winnerIndicator);
+
+    }
+
+    current_player = current_player === player_1 ? player_2 : player_1;
+
+}
+
+function playerwon() {
+
+
+    for (let i = 0; i < winningCombos.length; i++) {
+        const arr = winningCombos[i];
+
+        let firstindex = (array[arr[0]] === array[arr[1]]) && (array[arr[0]] !== null);
+        let middleindex = (array[arr[1]] === array[arr[2]]) && (array[arr[1]] !== null);
+        let lastindex = (array[arr[2]] === array[arr[0]]) && (array[arr[2]] !== null);
+
+
+        if (firstindex && lastindex && middleindex) {
+            winning_pattern = [arr[0], arr[1], arr[2]];
+            return true;
+        }
+    }
+    return false;
+}
+
+
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+
+
+document.getElementById('restart').addEventListener('click', restarted);
+
+
+function restarted() {
+    console.log("hi");
+    result.innerHTML = "Tic Tac Toc";
+    array.fill(null);
+
+    boxes.forEach(
+        x => {
+            x.innerHTML = '';
+            x.style.backgroundColor = '';
+        }
+    );
+   
+
+}
